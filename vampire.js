75d3781609197fb391d93e1973ecc5d1f77c0958
Lwong01 <43592585+Lwong01@users.noreply.github.com>
2019-02-08 12:32:35 -0800

    Create vampire.js

---
+++ vampire.js
@@ -0,0 +1,55 @@
+class Vampire {
+  constructor(name, yearConverted) {
+    this.name = name;
+    this.yearConverted = yearConverted;
+    this.offspring = [];
+    this.creator = null;
+  }
+
+
+  addOffspring(vampire) {
+    this.offspring.push(vampire);
+    vampire.creator = this;
+  }
+
+  // Returns the total number of vampires created by that vampire
+  get numberOfOffspring() {
+    return this.offspring.length;
+  }
+
+  // Returns the number of vampires away from the original vampire this vampire is
+  get numberOfVampiresFromOriginal() {
+    let numberOfVamp = 0;
+    let currentVamp = this;
+
+    while(currentVamp.creator){
+      currentVamp = currentVamp.creator;
+      numberOfVamp++;
+    }
+
+    return numberOfVamp;
+  }
+
+  // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
+  isMoreSeniorThan(vampire) {
+    if (this.numberOfVampiresFromOriginal < vampire.numberOfVampiresFromOriginal) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  /** Stretch **/
+
+  // Returns the closest common ancestor of two vampires.
+  // The closest common anscestor should be the more senior vampire if a direct ancestor is used.
+  // For example:
+  // * when comparing Ansel and Sarah, Ansel is the closest common anscestor.
+  // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
+  closestCommonAncestor(vampire) {
+
+  }
+}
+
+module.exports = Vampire;
+
